fe9ca36b907b8634f225a8f4c1d0b2c176591e3c
tjwagner27 <89555984+tjwagner27@users.noreply.github.com>
2022-03-30 16:35:53 -0400

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,20 @@
 import React from 'react';
+//Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+//Components
+import Grid from './Grid';
+import Spinner from './Spinner';
+//Hook
 
+//Image
+import NoImage from '../images/no_image.jpg';
 
-const Movie = () => <div>Movie</div>;
+const Movie = () => {
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  );
+};
 
 export default Movie;
