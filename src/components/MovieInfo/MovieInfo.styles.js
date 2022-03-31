b1489beefe7134a5d1e724035aa51ae2757e7d68
tjwagner27 <89555984+tjwagner27@users.noreply.github.com>
2022-03-31 19:15:37 -0400

    added movie info styles

--- src/components/MovieInfo/MovieInfo.styles.js
+++ src/components/MovieInfo/MovieInfo.styles.js
@@ -19,6 +19,54 @@ export const Wrapper = styled.div`
   }
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  background: rgba(0, 0, 0, 0.7);
+  border-radius: 20px;
 
-export const Text = styled.div``;
+  @media screen and (max-width: 768px) {
+    display: block;
+    max-height: none;
+  }
+`;
+
+export const Text = styled.div`
+  width: 100%;
+  padding: 20px 40px;
+  color: var(--white);
+  overflow: hidden;
+
+  .rating-directors {
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .score {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 35px;
+    height: 35px;
+    background: #fff;
+    color: #000;
+    font-weight: 800;
+    border-radius: 25px;
+    margin: 0;
+  }
+
+  .director {
+    margin: 0 0 0 40px;
+
+    p {
+      margin: 0;
+    }
+  }
+
+  h1 {
+    @media screen and (max-width: 768px) {
+      font-size: var(--fontBig);
+    }
+  }
+`;
