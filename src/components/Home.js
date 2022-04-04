a3c8a6b6cb9e9436b29bbe2016ea46ca238de73a
Tyler Wagner <t_wagner_27@hotmail.com>
2022-04-04 13:43:50 -0400

    refactored to use classes instead of hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 //API
 import API from '../API'
 // Config
@@ -10,54 +10,96 @@ import Thumb from './Thumb';
 import Spinner from './Spinner';
 import SearchBar from './SearchBar';
 import Button from './Button';
-// Hook
-import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
 import NoImage from '../images/no_image.jpg';
 
-const Home = () => {
-  const { state, 
-          loading, 
-          error, 
-          searchTerm, 
-          setSearchTerm, 
-          setIsLoadingMore 
-        } = useHomeFetch();
+const initialState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0
+};
 
-  console.log(state);
-  
-if (error) return <div>Something went wrong ...</div>
+class Home extends Component {
+  state = {
+    movies: initialState,
+    searchTerm: '',
+    isLoadingMore: false,
+    loading: false,
+    error: false,
+  }
 
-  return (
-    <>
-      {!searchTerm && state.results[0] ? (
-        <HeroImage
-          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-          title={state.results[0].original_title}
-          text={state.results[0].overview}
-        />
-      ) : null}
-      <SearchBar setSearchTerm={setSearchTerm} />
-      <Grid header={searchTerm ? 'Search Results' : 'Popular Movies'}>
-        {state.results.map(movie => (
-          <Thumb
-            key={movie.id}
-            clickable
-            image={
-              movie.poster_path
-              ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
-              : NoImage
-            }
-            movieId={movie.id}
-            />
-        ))}
-      </Grid>
-      {loading && <Spinner />}
-      {state.page < state.total_pages && !loading && (
-        <Button text='Load More' callback={() => setIsLoadingMore(true)}/>
-      )}
-    </>
-  );
+  fetchMovies = async (page, searchTerm="") => {
+    try {
+      this.setState({ error: false, loading: true });
+
+      const movies = await API.fetchMovies(searchTerm, page);
+
+      this.setState(prev=> ({
+        ...prev,
+        movies: {
+          ...movies,
+          results:
+            page > 1 ? [...prev.movies.results, ...movies.results] : [...movies.results]
+        },
+        loading: false,
+
+      }))
+    } catch (error) {
+      this.setState({ error: true, loading: false })
+    }
+  };
+
+handleSearch = searchTerm =>
+  this.setState({ movies: initialState, searchTerm }, () =>
+    this.fetchMovies(1, this.state.searchTerm)
+  )
+
+handleLoadMore = () =>
+  this.fetchMovies(this.state.movies.page + 1, this.state.searchTerm);
+
+componentDidMount() {
+  this.fetchMovies(1);
+}
+
+render() {
+
+  const { searchTerm, movies, loading, error } = this.state;
+
+  if (error) return <div>Something went wrong ...</div>
+
+    return (
+      <>
+        {!searchTerm && movies.results[0] ? (
+          <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movies.results[0].backdrop_path}`}
+            title={movies.results[0].original_title}
+            text={movies.results[0].overview}
+          />
+        ) : null}
+        <SearchBar setSearchTerm={this.handleSearch} />
+        <Grid header={searchTerm ? 'Search Results' : 'Popular Movies'}>
+          {movies.results.map(movie => (
+            <Thumb
+              key={movie.id}
+              clickable
+              image={
+                movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+              }
+              movieId={movie.id}
+              />
+          ))}
+        </Grid>
+        {loading && <Spinner />}
+        {movies.page < movies.total_pages && !loading && (
+          <Button text='Load More' callback={this.handleLoadMore}/>
+        )}
+      </>
+    );
+}
+
 };
 
 export default Home;
